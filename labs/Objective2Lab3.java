4b0dd243927981af883e9446a607834e85edd765
Christopher Rasmussen <Chris.E.Rasmussen1@gmail.com>
2023-10-16 17:56:50 +0200

    complete objective2lab3

---
+++ labs/Objective2Lab3.java
@@ -0,0 +1,13 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int birthyear = 1991;
+    String monthborn = "December";
+    boolean weekend = false;
+
+    System.out.println(birthyear);
+    System.out.println(monthborn);
+    System.out.println(weekend);
+
+
+  }
+}
