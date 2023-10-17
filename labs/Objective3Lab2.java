8e76e12833e9805a2692f054a2669ca77f422a9c
Christopher Rasmussen <Chris.E.Rasmussen1@gmail.com>
2023-10-17 23:49:31 +0200

    complete objective3lab2

---
+++ labs/Objective3Lab2.java
@@ -0,0 +1,12 @@
+public class Objective3Lab2 {
+	public static void main(String[] args){
+		double sideA, sideB, sideC;
+
+		sideA = 10;
+		sideB = 8;
+		sideC = Math.sqrt((sideA * sideA) + (sideB * sideB));
+
+		System.out.println("The hypotenuse of a triangle with sides 10.0 and 8.0 is " + sideC);
+
+	}
+}
